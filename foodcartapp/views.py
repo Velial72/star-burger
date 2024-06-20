0d2aa6cbda90114cd1419440b7a7a1bc1419bbf2
Anton <abramov1991a@gmail.com>
2024-06-20 22:48:03 +0500

    List orders into manager UI

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -65,62 +65,6 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-#     try:
-#         order = request.data
-#         ordered_items = order.get('products')
-#     except ValueError:
-#         return Response({'Error': 'ValueError'})
-#     if 'products' not in order:
-#         return Response({'Error': "'products' обязательное для заполнения поле"}, status=400)
-#     if not ordered_items:
-#         return Response({'Error': "'products' должен быть не пустым списком"}, status=400)
-#     if not isinstance(ordered_items, list):
-#         return Response({'Error': "'products' должен быть списком"}, status=400)
-#     required_fields = ['address', 'firstname', 'lastname', 'phonenumber']
-#     missing_fields = [field for field in required_fields if not order.get(field)]
-#     phonenumber = order.get('phonenumber')
-#     if missing_fields:
-#         return Response({'Error': f"Пропущен обязательный элемент: {', '.join(missing_fields)}"}, status=400)
-#     for field in required_fields:
-#         if not isinstance(order.get(field), str):
-#             return Response({'Error': f"{field}: Not a valid string"}, status=400)
-
-#     try:
-#         phone = PhoneNumber.from_string(phone_number=phonenumber, region='RU')
-#         if not phone.is_valid():
-#             raise phonenumbers.phonenumberutil.NumberParseException(0, "Введен некорректный номер телефона")
-#         phone = phone.as_e164
-#     except phonenumbers.phonenumberutil.NumberParseException:
-#         return Response({'Error': 'Введен некорректный номер телефона'}, status=400)
-
-
-    
-
-#     new_order = Order.objects.create(
-#         address=order.get('address'),
-#         name=order.get('firstname'),
-#         surname=order.get('lastname'),
-#         phone=phone,
-#     )
-
-#     for item in ordered_items:
-#         product_id = item.get('product')
-#         quantity = item.get('quantity')
-
-#         if quantity is None:
-#             return Response({'Error': 'Поле количество не может быть пустыми'}, status=400)
-#         try:
-#             ordered_product = Product.objects.get(id=item.get('product'))
-#             OrderProduct.objects.create(
-#                 order=new_order,
-#                 product=ordered_product,
-#                 quantity=item.get('quantity')
-#             )
-#         except Product.DoesNotExist:
-#             return Response({'Error': f"'products': недопустимый первичный ключ '{product_id}'"}, status=400)
-
-#     return Response(order)
-
     serializer = OrderSerializer(data=request.data)
     if serializer.is_valid():
         order = serializer.save()
@@ -133,10 +77,3 @@ def register_order(request):
         }
         return Response(response_data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-    # serializer = OrderSerializer(data=request.data)
-    # if serializer.is_valid():
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
